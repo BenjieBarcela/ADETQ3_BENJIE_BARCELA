b381f15d9cade602798fc3c9dd4ca9569b807150
User <User@DESKTOP-2P68FEM>
2020-10-23 16:03:38 +0100

    Commit a message

---
+++ ADETQ3_BENJIE_BARCELA/ADETQ3_BENJIE_BARCELA/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ADETQ3_BENJIE_BARCELA.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
